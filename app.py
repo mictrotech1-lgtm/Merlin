e94526e5d481cb6576b2c6c3d8fdd4364cc074eb
mictrotech1-lgtm <mictrotech1@gmail.com>
2026-08-17 00:38:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ sesion_activa = {
 }
 
 def cargar_conocimiento():
-    sab_path = os.path.join(BASE_DIR, "mictrotech.sab.txt")
+    sab_path = os.path.join(BASE_DIR, "mictrotech.sab")
     conocimiento = {}
     if os.path.exists(sab_path):
         with open(sab_path, "r", encoding="utf-8") as f:
